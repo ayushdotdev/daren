18e9c698e1e26ddffe3b7f157dd64a643071fe9f
Ayush <ayush438500@gmail.com>
2025-09-09 00:06:43 +0530

    moving to pycord due to support for dicord components v2

--- main.py
+++ main.py
@@ -1,32 +1,20 @@
-import os
+import discord
+import os 
 from dotenv import load_dotenv
-import nextcord
-from nextcord.ext import commands
 
-intents = nextcord.Intents.default()
-intents.message_content = True
+bot = discord.Bot()
 
-def get_prefix(bot,message):
-  return commands.when_mentioned_or("!")(bot,message)
-  
-bot = commands.Bot(command_prefix = get_prefix, intents = intents, help_command = None)
-
-cmds = [
-  "cogs.general",
-  "cogs.owner"
+cogs = [
+  "cogs.general"
   ]
 
 @bot.event
 async def on_ready():
-  print(f'logged in as {bot.user.name}')
-  await bot.sync_all_application_commands()
-  await bot.change_presence(
-    activity = nextcord.Game(name = "@Daren help")
-    )
-    
+  for cog in cogs:
+    bot.load_extension(cog)
+  print(f'Logged in as {bot.user}')
+  
 if __name__ == "__main__":
   load_dotenv()
-  bot_token = os.getenv('BOT_TOKEN')
-  for cmd in cmds:
-    bot.load_extension(cmd)
-  bot.run(bot_token)
+  token = os.getenv('BOT_TOKEN')
+  bot.run(token)

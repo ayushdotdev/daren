be683d205b281697ce9d19ded777082c80fc9074
Ayush <ayush438500@gmail.com>
2025-09-10 01:56:37 +0530

    status

--- bot.py
+++ bot.py
@@ -5,7 +5,6 @@ import os
 from dotenv import load_dotenv
 
 intents = discord.Intents.default()
-intents.message_content = True
 
 class Client(commands.Bot):
   async def setup_hook(self):
@@ -17,6 +16,9 @@ bot = Client(command_prefix = "!", intents = intents)
 
 @bot.event
 async def on_ready():
+  await bot.change_presence(
+    activity = discord.Activity(type = discord.ActivityType.listening, name = "/help")
+    )
   print(f"Logged in as {bot.user}")
   
 if __name__ == "__main__":

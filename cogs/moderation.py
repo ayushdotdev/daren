57f6502a0ce59264d8e4da082a2c09752aca85ae
Ayush <ayush438500@gmail.com>
2025-09-13 08:45:11 +0530

    softban cmd

--- cogs/moderation.py
+++ cogs/moderation.py
@@ -171,6 +171,67 @@ class Moderation(commands.Cog):
                 description="<:darenError:1415768665642766407> **_This user is not banned._**"
             )
             await interaction.response.send_message(embed=error, ephemeral=True)
+            
+    @app_commands.command(name="softban", description="Softban a member. (ban and then immediate unban to delete their messages")
+    @app_commands.checks.has_permissions(ban_members=True)
+    async def _softban(self, interaction: discord.Interaction, member: discord.Member, *, reason: str = "No reason provided"):
+        if member.id in [interaction.client.user.id, interaction.user.id, interaction.guild.owner_id]:
+            error = discord.Embed(
+                color=0xed2939,
+                description="<:darenError:1415768665642766407> **_I cannot kick this user._**"
+            )
+            await interaction.response.send_message(embed=error, ephemeral=True)
+            return
+    
+        if member.top_role.position >= interaction.guild.me.top_role.position:
+            error = discord.Embed(
+                color=0xed2939,
+                description="<:darenError:1415768665642766407> **_My role isn't high enough to moderate this user. Move my role up above others._**"
+            )
+        
+        if member.bot:
+          error = discord.Embed(
+            color=0xed2939,
+            description="<:darenError:1415768665642766407> **_I cannot kick other bots._**"
+          )
+          await interaction.response.send_message(embed=error, ephemeral=True)
+          return
+    
+        if member.top_role.position >= interaction.user.top_role.position and interaction.guild.owner_id != interaction.user.id:
+            error = discord.Embed(
+                color=0xed2939,
+                description="<:darenError:1415768665642766407> **_You cannot kick this user._**"
+            )
+            await interaction.response.send_message(embed=error, ephemeral=True)
+            return
+    
+        if not interaction.guild.me.guild_permissions.ban_members:
+            error = discord.Embed(
+                color=0xed2939,
+                description="<:darenError:1415768665642766407> **_I don’t have permission to ban members._**"
+            )
+            await interaction.response.send_message(embed=error, ephemeral=True)
+            return
+    
+        try:
+            await member.send(
+                embed=discord.Embed(
+                    title=f"Kicked from {interaction.guild.name}",
+                    description=f"You have been **kicked** from **{interaction.guild.name}**\n\n**Reason:**\n{reason}",
+                    color=0xff2400
+                )
+            )
+        except discord.Forbidden:
+            pass
+    
+        await interaction.guild.ban(member, reason = reason)
+        await interaction.guild.unban(member, reason = reason)
+    
+        success = discord.Embed(
+            color=0x48a860,
+            description=f"<:darenSuccess:1415789425652269096> **{member.name} was softbanned**"
+        )
+        await interaction.response.send_message(embed=success)
         
 
 async def setup(bot: commands.Bot):
